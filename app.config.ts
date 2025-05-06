dc7d65adfb136db37e0608bbd0e3de84dd8969a6
Diego Pagotto <diegodeabreupagotto@gmail.com>
2025-05-06 14:07:39 -0300

    refact: Uses .env for variables

---
+++ app.config.ts
@@ -0,0 +1,8 @@
+export default {
+    name: 'VirtualLP',
+    version: '1.0.0',
+    extra: {
+        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    },
+};
