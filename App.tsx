2233a04cbdc5663e8a39ecc5961139998439c60e
Diego Pagotto <diegodeabreupagotto@gmail.com>
2025-05-06 13:55:05 -0300

    feat: Changes default color scheme

--- App.tsx
+++ App.tsx
@@ -1,17 +1,48 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import { StyleSheet, View } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Profile" component={ProfileScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <View style={styles.container}>
+            <NavigationContainer theme={darkTheme}>
+                <Stack.Navigator
+                    initialRouteName="Login"
+                    screenOptions={{
+                        cardStyle: { backgroundColor: 'transparent' },
+                        headerStyle: { backgroundColor: '#000000' },
+                        headerTintColor: '#ffffff',
+                    }}
+                >
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name="Profile" component={ProfileScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </View>
     );
 }
+
+const darkTheme = {
+    ...DefaultTheme,
+    dark: true,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: 'white',
+        background: 'black',
+        card: 'black',
+        text: 'white',
+        border: 'gray',
+        notification: 'white',
+    },
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000000',
+    },
+});
